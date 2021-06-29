4cd10c59a038bca13b49485a5435cdacdd92ed09
steven <engineers2526@gmail.com>
2021-06-28 21:20:04 -0400

    Initial commit

---
+++ src/test/java/com/devhub/core/devhubdeveloperssocial/DevhubDevelopersSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.core.devhubdeveloperssocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhubDevelopersSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
